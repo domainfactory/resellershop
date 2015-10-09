442588256751298ed190971e6aa7985b85c1ddd4
Markus Langl <df-mlangl@users.noreply.github.com>
2015-10-09 10:25:22 +0200

    scss-compile unterstuetzt jetzt auch mit Parameter --watch

--- bin/scss-compile.sh
+++ bin/scss-compile.sh
@@ -2,10 +2,31 @@
 
 echo "Generiere CSS-Dateien. Dies kann einen Moment dauern, bitte haben Sie Geduld..."
 
+# Aufruf aus Hauptverzeichnis und in bin/ ermöglichen
+SOURCE="src/scss"
+DIST="dist/css"
+if [ -d $SOURCE ]; then
+  SOURCE=$SOURCE
+else
+  SOURCE="../$SOURCE"
+fi
+if [ -d $DIST ]; then
+  DIST=$DIST
+else
+  DIST="../$DIST"
+fi
+
+# Einen zusätzlichen SCSS-Parameter akzeptieren
+EXTRA_ARG=${1:-"nothing"}
+if [ $EXTRA_ARG == "nothing" ]; then
+  EXTRA_ARG=""
+fi
+
 LC_ALL=en_US.utf8 sass \
   --unix-newlines \
   --sourcemap=none \
   --style=compressed \
-  --load-path src/scss \
-  src/scss/shop.scss:dist/css/shop.css \
-  src/scss/fonts-sources.scss:dist/css/fonts-sources.css
+  --load-path $SOURCE \
+  $EXTRA_ARG \
+  $SOURCE/shop.scss:$DIST/shop.css \
+  $SOURCE/fonts-sources.scss:$DIST/fonts-sources.css
